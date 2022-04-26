c4ec117fcff3f7b82b8eb032e75f38af223f1ded
RaniaZarka <raniazarka@hotmail.com>
2022-04-26 18:28:59 +0200

    heroku added

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,20 @@
-module.exports = {
+
+// used for local database 
+/*module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "",
+    PASSWORD: "Rannout345",
     DB: "climatedb"
+  };*/
+
+  //used for heroku 
+
+  module.exports = {
+    HOST: "eu-cdbr-west-02.cleardb.net",
+    USER: "b3d5118c36b2ca",
+    PASSWORD: "90e8fb68",
+    DB: "heroku_c181751f7f98099"
   };
+
+
+  //b3d5118c36b2ca:90e8fb68@eu-cdbr-west-02.cleardb.net/heroku_c181751f7f98099?reconnect=true
