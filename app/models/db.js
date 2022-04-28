cef59473c98178c279b64f46dd2bc08d15c7d8b9
RaniaZarka <raniazarka@hotmail.com>
2022-04-28 18:44:36 +0200

    db

--- app/models/db.js
+++ app/models/db.js
@@ -7,10 +7,17 @@ const connection = mysql.createConnection({
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
+
+console.log ("connection is " + connection)
 // open the MySQL connection
 connection.connect(error => {
   if (error) throw error;
   console.log("<3 <3 <3 Successfully connected to our climatedb database <3 <3 <3");
   console.log(`==>> Go to ==>> http://localhost:8080/api/information`);
+
+  connection.on ('error', function(err){
+    if(err){console.log('2. error when connecting to db:', err);
+      }
+  })
 });
 module.exports = connection;
