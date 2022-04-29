c9085428969e37c341fd9fd18045f005b1e6da7d
RaniaZarka <raniazarka@hotmail.com>
2022-04-29 12:59:43 +0200

    db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,20 +1,20 @@
 
 // used for local database 
- module.exports = {
+/*  module.exports = {
     HOST: "localhost",
     USER: "root",
     PASSWORD: "Rannout345",
     DB: "climatedb"
-  };
+  }; */
 
   //used for heroku 
 
-   /*module.exports = {
+   module.exports = {
     HOST: "eu-cdbr-west-02.cleardb.net",
     USER: "b3d5118c36b2ca",
     PASSWORD: "90e8fb68",
     DB: "heroku_c181751f7f98099"
-  }; */
+  }; 
 
 
   //b3d5118c36b2ca:90e8fb68@eu-cdbr-west-02.cleardb.net/heroku_c181751f7f98099?reconnect=true
