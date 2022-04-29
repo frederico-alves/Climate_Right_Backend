4aae1ee4aca00d64ff5813c948fbc89ad428b60f
RaniaZarka <raniazarka@hotmail.com>
2022-04-29 12:17:42 +0200

    db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -10,10 +10,10 @@
   //used for heroku 
 
   module.exports = {
-    HOST: "clwxydcjair55xn0.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-    USER: "b	vrieecs4t6susxxh",
-    PASSWORD: "	y4mdsd94z50n5tef",
-    DB: "	l4gntuf0uwtayehi"
+    HOST: "eu-cdbr-west-02.cleardb.net",
+    USER: "b3d5118c36b2ca",
+    PASSWORD: "90e8fb68",
+    DB: "heroku_c181751f7f98099"
   };
 
 
